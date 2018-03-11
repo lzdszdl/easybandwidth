00122c785493def7f73286e9c530187b2d73e9cd
lzdszdl <136208872@qq.com>
2018-03-11 16:32:19 +0800

    Create bandwidth.sh

---
+++ bandwidth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MAX="11000"
+A=`/sbin/iptables -n -v -L -t filter |grep -i --color -B 3 spt:8080|awk -F' ' '{print $7}'`
+B=`echo ${A%o*}`
+echo $B > /root/bandwidth.txt
+C=`echo ${B%M*}`
+if [ $C -gt $MAX ]; then
+  /etc/init.d/shadowsocks stop
+ # iptables -D OUTPUT -s 192.168.0.4 -p tcp --sport $PORT
+ fi
+#MAX=最大流量，单位M 本实例为监控8080端口流量，超出流量则结束某进程
