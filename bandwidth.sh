efddc61794a8a2c37291c5879c95fa1b3bba880e
lzdszdl <136208872@qq.com>
2018-03-11 16:33:10 +0800

    Update bandwidth.sh

--- bandwidth.sh
+++ bandwidth.sh
@@ -6,6 +6,8 @@ echo $B > /root/bandwidth.txt
 C=`echo ${B%M*}`
 if [ $C -gt $MAX ]; then
   /etc/init.d/shadowsocks stop
- # iptables -D OUTPUT -s 192.168.0.4 -p tcp --sport $PORT
+ # iptables -D OUTPUT -s 127.0.0.1 -p tcp --sport $PORT
  fi
 #MAX=最大流量，单位M 本实例为监控8080端口流量，超出流量则结束某进程
+
+#第一次使用请先执行 iptables -I OUTPUT -s 127.0.0.1 -p tcp --sport 8080
